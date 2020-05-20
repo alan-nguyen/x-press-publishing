209cec987e1833e6d0cbc5aa8a1f0f3b5a1ae0b7
Alan Nguyen <tuan8101@gmail.com>
2020-05-19 21:21:24 -0600

    Create Artist table

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorhandler = require('errorhandler');
 const express = require('express');
+const morgan = require('morgan');
+
 const apiRouter = require('./api/api');
-app.use('/api', apiRouter);
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(errorhandler());
 app.use(morgan('dev'));
+
+app.use('/api', apiRouter);
+
+// Errorhandler should be at the bottom
+app.use(errorhandler());
 
 app.listen(POST, () => {
   console.log(`Listening on port: ${PORT}`);
