360432ee3c598b689dc148b0d1cae139ca673920
Alan Nguyen <tuan8101@gmail.com>
2020-05-19 21:05:39 -0600

    Use cors, errorhandler and morgan

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
+const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
+app.use(cors());
+app.use(errorhandler());
+app.use(morgan('dev'));
 
 app.listen(POST, () => {
   console.log(`Listening on port: ${PORT}`);
