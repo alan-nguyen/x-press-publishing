c7d1c0298ea62a715be2cd61999dd2c60a44d6ee
Alan Nguyen <tuan8101@gmail.com>
2020-05-19 21:44:20 -0600

    Create artistsRouter

--- api/api.js
+++ api/api.js
@@ -2,3 +2,4 @@ const express = require('express');
 const apiRouter = express.Router();
 
 module.exports = apiRouter;
+const artistsRouter = require('./artists');
---
+++ api/artists.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const artistsRouter = express.Router();
+const sqlite3 = require('sqlite3');
+
+module.exports = artistsRouter;
