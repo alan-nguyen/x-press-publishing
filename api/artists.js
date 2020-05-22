02a015f60b1dccf782f3eaa1f9926d4f23c8ba78
Alan Nguyen <tuan8101@gmail.com>
2020-05-21 20:54:59 -0600

    Retrieve all entries in teh Artist table where is_currently_employed = 1

--- api/artists.js
+++ api/artists.js
@@ -7,3 +7,11 @@ module.exports = artistsRouter;
 const db = new sqlite3.Database(
   process.env.TEST_DATABASE || './database.sqlite'
 );
+
+// GET handler to the router for the / path
+artistsRouter.get('/', (req, res, next) => {
+  db.all(
+    'SELECT * FROM Artist WHERE  Artist.is_currently_employed = 1',
+    (err, artists) => {}
+  );
+});
