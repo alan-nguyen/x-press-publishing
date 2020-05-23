762ebc4110bd6b80ac2b412ecc68eda595d6c969
Alan Nguyen <tuan8101@gmail.com>
2020-05-22 23:45:16 -0600

    Check if an artist was retrieved

--- api/api.js
+++ api/api.js
@@ -7,5 +7,14 @@ const artistsRouter = require('./artists');
 artistsRouter.param('artistId', (req, res, next, artistId) => {
   const sql = 'SELECT * FROM Artist WHERE Artist.id = $artistId';
   const values = { $artistId: artistId };
-  db.get(sql, values, (error, artist) => {});
+  db.get(sql, values, (error, artist) => {
+    if (error) {
+      next(error);
+    } else if (artist) {
+      req.artist = artist;
+      next();
+    } else {
+      res.sendStatus(404);
+    }
+  });
 });
