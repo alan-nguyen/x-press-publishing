0ae7904fe63e8f405cc2fb391724dffb288537d3
Alan Nguyen <tuan8101@gmail.com>
2020-05-23 00:48:17 -0600

    Move requests to artist

--- api/api.js
+++ api/api.js
@@ -1,35 +1,8 @@
 const express = require('express');
 const apiRouter = express.Router();
 
-const artistsRouter = require('./artists');
+const artistsRouter = require('./artists.js');
 
-artistsRouter.param('artistId', (req, res, next, artistId) => {
-  const sql = 'SELECT * FROM Artist WHERE Artist.id = $artistId';
-  const values = { $artistId: artistId };
-  db.get(sql, values, (error, artist) => {
-    if (error) {
-      next(error);
-    } else if (artist) {
-      req.artist = artist;
-      next();
-    } else {
-      res.sendStatus(404);
-    }
-  });
-});
-
-// GET handler to the router for the / path
-artistsRouter.get('/', (req, res, next) => {
-  db.all(
-    'SELECT * FROM Artist WHERE  Artist.is_currently_employed = 1',
-    (err, artists) => {
-      if (err) {
-        next(err);
-      } else {
-        res.status(200).json({ artists: artists });
-      }
-    }
-  );
-});
+apiRouter.use('/artist', artistsRouter);
 
 module.exports = apiRouter;
