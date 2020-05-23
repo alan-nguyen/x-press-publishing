ba48de1ad7f219bd36aa296632b25619d76ecbd8
Alan Nguyen <tuan8101@gmail.com>
2020-05-22 23:43:25 -0600

    Execute SQL query to get  the artist with the given ID

--- api/api.js
+++ api/api.js
@@ -4,4 +4,8 @@ const apiRouter = express.Router();
 module.exports = apiRouter;
 const artistsRouter = require('./artists');
 
-artistsRouter.param('artistId', (req, res, next, artistId) => {});
+artistsRouter.param('artistId', (req, res, next, artistId) => {
+  const sql = 'SELECT * FROM Artist WHERE Artist.id = $artistId';
+  const values = { $artistId: artistId };
+  db.get(sql, values, (error, artist) => {});
+});
